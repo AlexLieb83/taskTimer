b328dc38014b2544222a24591fb85bc207d2340e
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-03-22 09:14:36 -0500

    test

---
+++ script.js
@@ -0,0 +1,29 @@
+const vacation = "6 April 2023";
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+function countdown() {
+  const vacationDate = new Date(vacation);
+  const currentDate = new Date();
+
+  const totalSeconds = (vacationDate - currentDate) / 1000;
+  const seconds = Math.floor(totalSeconds) % 60;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
